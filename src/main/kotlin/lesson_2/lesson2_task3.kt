45ac35fe843f9a2043c82f0d2f1f0c62b1f2d885
Виктор Лобынцев <sibfight@mail.ru>
2025-10-15 17:21:46 +0300

    KS-2-3 вынес константы

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,13 +1,16 @@
 package org.example.lesson_2
 
-fun main(){
-    val hours = 9
-    val minutes = 39
-    val timePathToMinutes = 457
+const val MINUTES_IN_HOUR = 60
+const val HOURS_IN_DAY = 24
 
-    val totalMinutes = hours * 60 + minutes + timePathToMinutes
-    val arrivalHours = (totalMinutes / 60) % 24
-    val arrivalMinutes = totalMinutes % 60
+fun main() {
+    val departureHours = 9
+    val departureMinutes = 39
+    val travelTimeMinutes = 457
+
+    val totalMinutes = departureHours * MINUTES_IN_HOUR + departureMinutes + travelTimeMinutes
+    val arrivalHours = (totalMinutes / MINUTES_IN_HOUR) % HOURS_IN_DAY
+    val arrivalMinutes = totalMinutes % MINUTES_IN_HOUR
 
     println("Поезд прибудет в: ${String.format("%02d:%02d", arrivalHours, arrivalMinutes)}")
 }
