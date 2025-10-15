fc4ad6c68f9c2ad3cd5cf57d170f876f747115fe
Виктор Лобынцев <sibfight@mail.ru>
2025-10-15 17:32:41 +0300

    KS-2-3 вынес TOTAL_SECONDS_IN_SPACE

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,13 +2,13 @@ package org.example.lesson_1
 
 const val SECONDS_IN_MINUTE = 60
 const val SECONDS_IN_HOUR = 3600
+const val TOTAL_SECONDS_IN_SPACE = 6480
 
 fun main() {
-    val totalSeconds = 6480
 
-    val hours = totalSeconds / SECONDS_IN_HOUR
-    val minutes = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
-    val seconds = totalSeconds % SECONDS_IN_MINUTE
+    val hours = TOTAL_SECONDS_IN_SPACE / SECONDS_IN_HOUR
+    val minutes = (TOTAL_SECONDS_IN_SPACE % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds = TOTAL_SECONDS_IN_SPACE % SECONDS_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
