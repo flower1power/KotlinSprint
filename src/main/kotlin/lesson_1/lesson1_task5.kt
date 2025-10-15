ca624b16a66ba6c49ae31b3ed126b0076275b7c5
Виктор Лобынцев <sibfight@mail.ru>
2025-10-15 17:19:37 +0300

    KS-1-5 вынес константы

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,11 +1,14 @@
 package org.example.lesson_1
 
-fun main(){
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
+
+fun main() {
     val totalSeconds = 6480
 
-    val hours = totalSeconds / 3600
-    val minutes = (totalSeconds % 3600) / 60
-    val seconds = totalSeconds % 60
+    val hours = totalSeconds / SECONDS_IN_HOUR
+    val minutes = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds = totalSeconds % SECONDS_IN_MINUTE
 
-    println(String.format("%02d:%02d:%02d", hours, minutes ,seconds))
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
