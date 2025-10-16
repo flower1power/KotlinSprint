397b3c1abe7e220817631e0b27aedb7eb4d179ee
Виктор Лобынцев <sibfight@mail.ru>
2025-10-16 15:49:52 +0300

    KS-2-5 rate инициирован целым числом - замет перевод в %

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,9 +5,10 @@ import kotlin.math.pow
 fun main() {
     val years = 20
     val initialAmount = 70_000
-    val rate = 0.167
+    val rate = 167
+    val rateToPercent = rate.toDouble() / 100
     val timesPerYear = 1
-    val resultAmount = initialAmount * (1 + rate / timesPerYear).pow(timesPerYear * years)
+    val resultAmount = initialAmount * (1 + rateToPercent / timesPerYear).pow(timesPerYear * years)
 
     println(String.format("%.3f", resultAmount))
     assert(resultAmount == 1536438.428)
