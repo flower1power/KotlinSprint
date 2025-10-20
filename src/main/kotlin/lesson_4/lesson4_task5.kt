c5349eacc8239ea5a6fab30ff902b8b72327e509
Виктор Лобынцев <sibfight@mail.ru>
2025-10-20 17:25:35 +0300

    KS-4-5 -добавил константы для логических операторов

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -5,6 +5,9 @@ const val MAX_CREW = 70
 const val RECOMMENDED_CREW = 70
 const val MIN_PROVISIONS_NORMAL = 50
 const val MIN_PROVISIONS_WITH_DAMAGE = 50
+const val SHIP_MUST_BE_UNDAMAGED = true
+const val WEATHER_MUST_BE_GOOD = true
+
 
 fun main() {
     print("Есть повреждения корпуса? (true/false): ")
@@ -19,12 +22,10 @@ fun main() {
     print("Благоприятная погода? (true/false): ")
     val isWeatherGood = readln().toBoolean()
 
-    val canSail = (!hasDamage &&
-            crewCount in MIN_CREW..MAX_CREW &&
-            provisionsCount > MIN_PROVISIONS_NORMAL) ||
-            (hasDamage && crewCount == RECOMMENDED_CREW &&
-                    isWeatherGood &&
-                    provisionsCount >= MIN_PROVISIONS_WITH_DAMAGE)
+
+    val canSail =
+        (hasDamage != SHIP_MUST_BE_UNDAMAGED && crewCount in MIN_CREW..MAX_CREW && provisionsCount > MIN_PROVISIONS_NORMAL) ||
+                (hasDamage == SHIP_MUST_BE_UNDAMAGED && crewCount == RECOMMENDED_CREW && isWeatherGood == WEATHER_MUST_BE_GOOD && provisionsCount >= MIN_PROVISIONS_WITH_DAMAGE)
 
     println("Корабль может отправиться в плавание: $canSail")
 }
