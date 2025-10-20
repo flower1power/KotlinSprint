3a56eaa2a845c899791deeb368db3c2652a302f9
Виктор Лобынцев <sibfight@mail.ru>
2025-10-20 17:26:50 +0300

    KS-4-1 у убрал константу

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,12 +1,10 @@
 package org.example.lesson_4
 
-const val TOTAL_TABLES = 13
-
 fun main() {
     val reservedTablesToday = 13
     val reservedTablesTomorrow = 9
 
     println(
-        "Доступность столиков на сегодня: ${reservedTablesToday < TOTAL_TABLES}\nДоступность столиков на завтра: ${reservedTablesTomorrow < TOTAL_TABLES}"
+        "Доступность столиков на сегодня: ${reservedTablesToday < 13}\nДоступность столиков на завтра: ${reservedTablesTomorrow < 13}"
     )
 }
