03d3791f5e057b206a5cfd7435c140648495c5f0
Виктор Лобынцев <sibfight@mail.ru>
2025-10-20 19:18:15 +0300

    KS-5-2 - проверка возраста

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_5
+
+import java.time.LocalDate
+
+const val AGE_OF_MAJORITY = 18
+const val FULL_ACCESS_MSG = "Показать экран со скрытым контентом"
+const val PARTIAL_ACCESS_MSG = "Показать часть контента"
+const val NO_ACCESS_MSG = "Вернуть на базовый экран"
+
+fun main() {
+    val currentYear = LocalDate.now().year
+
+    val greetingMsg = "Введите год рождения (1999):"
+    print(greetingMsg)
+
+    val userYear = readln().toInt()
+    val userAge = currentYear - userYear
+
+    val msg = when (userAge) {
+        in AGE_OF_MAJORITY..Int.MAX_VALUE -> FULL_ACCESS_MSG
+        16, 17 -> PARTIAL_ACCESS_MSG
+        else -> NO_ACCESS_MSG
+    }
+
+    print(msg)
+}
