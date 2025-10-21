35965d691a15d6551bd0c76b907e5d0daf615242
Виктор Лобынцев <sibfight@mail.ru>
2025-10-21 18:45:59 +0300

    KS-5-3 - убрал импорт, вынес константы, реализация через if else

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,25 +1,27 @@
 package org.example.lesson_5
 
-import kotlin.random.Random
+const val MIN_NUMBER = 0
+const val MAX_NUMBER = 42
 
 fun main() {
-    val firstRandomNumber = Random.nextInt(0, 43)
-    val secondRandomNumber = Random.nextInt(0, 43)
+    val firstRandomNumber = (MIN_NUMBER..MAX_NUMBER).random()
+    val secondRandomNumber = (MIN_NUMBER..MAX_NUMBER).random()
 
-    println("Введите первое число от 0 до 42 включительно: ")
+    println("Введите первое число от $MIN_NUMBER до $MAX_NUMBER включительно: ")
     val firstNumber = readln().toInt()
-    println("Введите второе число от 0 до 42 включительно: ")
+    println("Введите второе число от $MIN_NUMBER до $MAX_NUMBER включительно: ")
     val secondNumber = readln().toInt()
 
     val isFirstMatch = firstNumber == firstRandomNumber || firstNumber == secondRandomNumber
     val isSecondMatch = secondNumber == firstRandomNumber || secondNumber == secondRandomNumber
 
-    val msg = when {
-        isFirstMatch && isSecondMatch -> "Поздравляем! Вы выиграли главный приз!"
-        isFirstMatch || isSecondMatch -> "Вы выиграли утешительный приз!"
-        else -> "Неудача!"
+    if (isFirstMatch && isSecondMatch) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (isFirstMatch || isSecondMatch) {
+        println("Вы выиграли утешительный приз!")
+    } else {
+        println("Неудача!")
     }
 
-    println(msg)
     println("Правильные числа для победы: $firstRandomNumber и $secondRandomNumber")
 }
