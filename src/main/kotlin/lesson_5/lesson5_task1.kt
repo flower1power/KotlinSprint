49ba5f14315f75a49aa889c4a57ed25b2bbd581c
Виктор Лобынцев <sibfight@mail.ru>
2025-10-21 17:46:14 +0300

    KS-5-1 - вынес переменные для уравнения и автоматический подсчет верного ответа

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,15 +1,16 @@
 package org.example.lesson_5
 
-const val GREETING_MESSAGE = "2 + 2 * 2 ="
-const val EXPECTED_SOLUTION = 6
 const val ACCESS_GRANTED = "Добро пожаловать!"
 const val ACCESS_DENIED = "Доступ запрещен."
 
 fun main() {
-    print("Запишите ответ для уравнения: $GREETING_MESSAGE: ")
+    val leftNum = 2
+    val rightNum = 3
+    val expectedSolution = leftNum + rightNum
+    print("Запишите ответ для уравнения: $leftNum + $rightNum: ")
     val solution = readln().toInt()
 
-    if (solution == EXPECTED_SOLUTION) {
+    if (solution == expectedSolution) {
         print(ACCESS_GRANTED)
     } else {
         print(ACCESS_DENIED)
