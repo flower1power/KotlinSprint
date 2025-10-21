be2d8aaa7f62e724b1529b4f6a9b9cccd304f584
Виктор Лобынцев <sibfight@mail.ru>
2025-10-21 19:00:42 +0300

    KS-5-4 - вынес константы и сообщения в переменные

--- src/main/kotlin/lesson_5/lesson5_task4.kt
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -1,38 +1,43 @@
 package org.example.lesson_5
 
+const val USER_NAME = "Zaphod"
+const val USER_PASSWORD = "PanGalactic"
 fun main() {
-    val userName = "Zaphod"
-    val userPassword = "PanGalactic"
-
-    println(
-        """
+    val greetingMsgMarvin = """
         Внимание, пассажир.
         Моя обязанность, как обычно, незавидная, - приветствовать вас на борту корабля "Heart of Gold".
         [вздыхает...] Что ж, введи, пожалуйста, свое имя пользователя, чтобы мы могли приступить к процессу входа.
         Я всегда готов служить, хотя это не приносит мне радости... но вы, наверное, об этом не заботитесь.
         [вздыхает ещё глубже...] Да, вперед, пожалуйста, вводите свои данные...
-    """.trimIndent()
-    )
+        """.trimIndent()
 
-    println("\nВведите логин:")
+    val askLoginMsg = "\nВведите логин: "
+    val wrongUserMsg = "Пользователь не существует, прошу зарегистрироваться."
+
+    val askPasswordMsg = "Введите пароль: "
+
+    val successMsgMarvin = """
+        
+        [вздыхает...] Ваши данные проверены, и о, чудо, они верны... 
+        Пользователь $USER_NAME, вам разрешено входить на борт корабля "Heart of Gold". Хотя мне всё равно... 
+        Ну вперед, войдите... 
+        Если вам так уж надо, в конце концов... 
+        [меланхолический вздох...] Надеюсь, вам понравится пребывание здесь больше, чем мне.
+        """.trimIndent()
+
+
+    println(greetingMsgMarvin)
+
+    print(askLoginMsg)
     val login = readln()
-    if (login != userName) {
-        println("Пользователь не существует, прошу зарегистрироваться")
+    if (login != USER_NAME) {
+        println(wrongUserMsg)
         return
     }
 
-    println("\nВведите пароль:")
+    print(askPasswordMsg)
     val password = readln()
-
-    if (password == userPassword) {
-        println(
-            """
-            [вздыхает...] Ваши данные проверены, и о, чудо, они верны... 
-            Пользователь "Zaphod", вам разрешено входить на борт корабля "Heart of Gold". Хотя мне всё равно... 
-            Ну вперед, войдите... 
-            Если вам так уж надо, в конце концов... 
-            [меланхолический вздох...] Надеюсь, вам понравится пребывание здесь больше, чем мне.
-        """.trimIndent()
-        )
+    if (password == USER_PASSWORD) {
+        println(successMsgMarvin)
     }
 }
